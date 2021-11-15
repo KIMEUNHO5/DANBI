032ddfd55516cef20ffd173c7c27d81043ee4686
UNO <applejam533@gmail.com>
2021-11-15 22:35:19 +0900

    Main mandurotta!

--- App.js
+++ App.js
@@ -2,26 +2,50 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Image, Text, View, Button } from 'react-native';
 
 const Stack = createStackNavigator();
 
 // Screens
 import LoginScreen from "./screens/LoginScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import mainScreen from "./screens/Main";
 
 
 //React Navigation Setup
 import { NavigationContainer } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+      <Button
+        title="Go to Main"
+        onPress={() => navigation.navigate('Main')}
+      />
+    </View>
+  );
+}
 
 const App = () => {
+  
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Main" component={mainScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
 
 export default App;
--- screens/Main.js
+++ screens/Main.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 import {Image} from 'react-native';
-const mainScreen = () => {
+
+const mainScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
             <View style={styles.header}>
-            <Image
-            source={require('assets\icon.png')}
-          />
+                <Text>Hello!</Text>
             </View>
         </View>
-      );
+    );
 }
 
+/*
+<Image source={require('assets\icon.png')} />
+*/
 export default mainScreen;
 const styles = StyleSheet.create({
     container : {
