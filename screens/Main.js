51f30efd64e7a04ab5a354c8fd75b66df7c8a8da
SJ <tnwjd5127@naver.com>
2021-11-16 13:27:30 +0900

    change image name

--- screens/Main.js
+++ screens/Main.js
@@ -2,13 +2,15 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import {Image} from 'react-native';
+import { Header } from "react-native/Libraries/NewAppScreen";
+
 
 const mainScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
             <View style={styles.header}>
-                <Image style={styles.logo} source={require('../Source/DANBI_Logo.png')} />
+                <Image style={styles.logo} source={require('../Source/DANBI_Logo+Name.png')} />
             </View>
         </View>
     );
