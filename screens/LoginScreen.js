d90356e4572f9f7d14c4be65adcaaeac6036b885
UNO <applejam533@gmail.com>
2021-11-16 10:25:51 +0900

    loginscreen update

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, View, Button, Image, Text, TextInput, TouchableOpacity, ColorPropType } from "react-native";
 import * as Google from "expo-google-app-auth";
+//import logo from './Source/DANBI_Logo.png'; 
 
 const LoginScreen = ({ navigation }) => {
   const signInAsync = async () => {
@@ -23,11 +24,95 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../Source/DANBI_Logo.png')}
+      />
+      <Image
+        style={styles.name}
+        source={require('../Source/DANBI_Name.png')}
+      />
+      <View style={styles.TextLoginContainer}>
+        <Text
+          style={styles.TextLogin}>
+          Login
+        </Text>
+      </View>
+      <View style={styles.InputAndButton}>
+        <View style={styles.Inputcontainer}>
+          <TextInput
+            placeholder={"Email"}
+            style={styles.Input}
+          /> 
+          <TextInput
+            placeholder={"Password"}
+            style={styles.Input}
+          />             
+        </View>
+        <TouchableOpacity
+          style={styles.LoginButton}>
+            <Text style={{color: "#FFFFFF", fontSize: 16, fontWeight: "600"}}
+            >Login</Text>
+        </TouchableOpacity>
+      </View>
+      
       <Button title="Login with Google" onPress={signInAsync} />
+      
+
     </View>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "white",
+  },
+  logo: {
+    width: 120,
+    height: 140,
+    marginTop: 40,
+    resizeMode: "contain",
+  },
+  name: {
+    width: 150,
+    height: 50,
+    marginBottom: 60,
+    resizeMode: "contain",
+  },
+  TextLoginContainer:{
+    alignSelf: "stretch",
+    marginHorizontal: 30,
+  },
+  TextLogin:{
+    fontSize: 40,
+    fontWeight: "800",
+  },
+  InputAndButton:{
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 30,
+    backgroundColor: "orange",
+    marginBottom: 70,
+  },
+  Inputcontainer:{
+    flexDirection: "column",
+  },
+  Input:{
+    fontSize: 18,
+  },
+  LoginButton:{
+    width: 85,
+    height: 90,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor: "#7E7E7E",
+    padding: 10,
+    borderRadius: 10,
+  },
+});
