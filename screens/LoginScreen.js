c8e28c8c1fa23c517481084616f1533fd7ad2195
KIMEUNHO5 <applejam533@gmail.com>
2021-11-19 11:00:09 +0900

    account check

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -57,7 +57,7 @@ const LoginScreen = ({ navigation }) => {
         </View>
         <TouchableOpacity style={styles.LoginButton}>
             <Text style={{color: "#FFFFFF", fontSize: 16, fontWeight: "600"}}>
-              Login
+              Login!!!!!!!!!!
             </Text>
         </TouchableOpacity>
       </View>
@@ -134,6 +134,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     //backgroundColor: "orange",
     marginBottom: 70,
+
   },
   Inputcontainer:{
     flexDirection: "column",
